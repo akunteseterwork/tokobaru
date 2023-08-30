95907436eb09c5f46178c89f92f095087ed8b00c
hilalmustofa <hilal.mustofa90@gmail.com>
2023-08-30 11:57:41 +0700

    add export config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'tokobaru.alwaysdata.net/v1',
-            pathname: '/uploads/**',
-          },
-        ],
+  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tokobaru.alwaysdata.net/v1',
+        pathname: '/uploads/**',
       },
+    ],
+  },
 }
 
 module.exports = nextConfig
