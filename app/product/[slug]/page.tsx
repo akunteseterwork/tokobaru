9fb559765f993a819612c83ae8aaee25b9820bb7
hilalmustofa <hilal.mustofa90@gmail.com>
2023-08-30 14:19:16 +0700

    try to fix page

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,16 +1,52 @@
-"use client";
 import React, { useState } from 'react';
-import useSWR from 'swr';
-import { fetchData, fetchWithToken } from '../../../utils/fetcher';
+import { fetchWithToken } from '../../../utils/fetcher';
 import { FaShoppingCart, FaMinus, FaPlus } from 'react-icons/fa';
 import Image from 'next/image';
 import AddToCart from '../../../components/modals/addToCartModal';
+import { GetStaticPaths, GetStaticProps } from 'next';
 
+export const getStaticPaths: GetStaticPaths = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products`);
+  const data = await response.json();
+  
+  const paths = data.map((product: any) => ({
+    params: { slug: product.id.toString() }, 
+  }));
+  
+  return {
+    paths,
+    fallback: true, 
+  };
+};
 
-export default function Detail({ params }: { params: { slug: string } }) {
-  const { data, error } = useSWR(
-    `${process.env.NEXT_PUBLIC_API_URL}/products/${params.slug}`, fetchData
-  );
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const slug = params?.slug;
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/products/${slug}`);
+  const data = await response.json();
+  
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+interface ProductDetail {
+  id: number;
+  name: string;
+  picture: string;
+  description: string;
+  price: number;
+  stock: number;
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface DetailProps {
+  data: ProductDetail;
+}
+
+export default function Detail({ data }: DetailProps) {
   const [quantity, setQuantity] = useState(1);
   const [modalOpen, setModalOpen] = useState(false);
 
@@ -20,14 +56,14 @@ export default function Detail({ params }: { params: { slug: string } }) {
 
   const handleAddToCart = async () => {
     try {
-      await fetchWithToken(`${process.env.NEXT_PUBLIC_API_URL}/orders/${params.slug}`, {
+      await fetchWithToken(`${process.env.NEXT_PUBLIC_API_URL}/orders/${data.id}`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ amount: quantity }),
       });
-      
+
       setModalOpen(true);
     } catch (error) {
       console.error(error);
@@ -36,12 +72,12 @@ export default function Detail({ params }: { params: { slug: string } }) {
 
   const handleCloseModal = () => {
     setModalOpen(false);
-  }
+  };
 
-  const formatRupiah = (money:number) => {
-    return new Intl.NumberFormat("id-ID", {
-      style: "currency",
-      currency: "IDR",
+  const formatRupiah = (money: number) => {
+    return new Intl.NumberFormat('id-ID', {
+      style: 'currency',
+      currency: 'IDR',
       minimumFractionDigits: 0,
     }).format(money);
   };
@@ -52,36 +88,25 @@ export default function Detail({ params }: { params: { slug: string } }) {
     }
   };
 
-  if (error) return <div className="text-center mt-8">Error loading data</div>;
-  if (!data)
-    return (
-      <div className="flex bg-gray-100 justify-center items-center w-full h-screen">
-        <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-blue-500 border-opacity-50"></div>
-      </div>
-    );
-
   return (
     <div className="bg-gray-100 flex justify-center items-center p-8">
       <div className="max-w-4xl w-full p-2 rounded-lg">
         <div className="flex flex-col md:flex-row items-center">
           <div className="md:w-1/3 mb-4 md:mb-0 md:mr-8">
             <Image
-              src={data.data.picture}
-              alt={data.data.name}
+              src={data.picture}
+              alt={data.name}
               width={500}
               height={500}
               className="shadow-image-detail"
             />
           </div>
           <div className="w-full md:w-6/12">
-            <h1 className="text-2xl font-semibold text-gray-800">{data.data.name}</h1>
-            <p className="text-gray-600">{formatRupiah(data.data.price)}</p>
-            <p className="text-gray-500 mt-2">In Stock: {data.data.stock}</p>
+            <h1 className="text-2xl font-semibold text-gray-800">{data.name}</h1>
+            <p className="text-gray-600">{formatRupiah(data.price)}</p>
+            <p className="text-gray-500 mt-2">In Stock: {data.stock}</p>
             <div className="mt-4 flex items-center">
-              <button
-                onClick={handleDecrement}
-                className="text-red-600"
-              >
+              <button onClick={handleDecrement} className="text-red-600">
                 <FaMinus />
               </button>
               <input
@@ -95,42 +120,30 @@ export default function Detail({ params }: { params: { slug: string } }) {
                 }}
                 className="w-12 text-center mx-2 text-black border-blue-300 rounded"
               />
-              <button
-                onClick={handleIncrement}
-                className="text-green-600"
-              >
+              <button onClick={handleIncrement} className="text-green-600">
                 <FaPlus />
               </button>
             </div>
-            <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md flex items-center" onClick={handleAddToCart}>
+            <button
+              className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md flex items-center"
+              onClick={handleAddToCart}
+            >
               <FaShoppingCart className="mr-2" />
               Add to Cart
             </button>
             <div className="mt-4">
-              <p className="text-gray-700">{data.data.description}</p>
+              <p className="text-gray-700">{data.description}</p>
             </div>
           </div>
           {modalOpen && (
-        <AddToCart title="Add to Cart Success" message="You can now checkout or close this to continue shopping"  onClose={handleCloseModal}/>
-      )}
+            <AddToCart
+              title="Add to Cart Success"
+              message="You can now checkout or close this to continue shopping"
+              onClose={handleCloseModal}
+            />
+          )}
         </div>
       </div>
     </div>
   );
-}
-
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: 'blocking'
-  };
-}
-
-export async function getStaticProps({ params }: { params: { slug: string } }) {
-  const product = { slug: params.slug };
-  return {
-    props: {
-      product,
-    },
-  };
 }
