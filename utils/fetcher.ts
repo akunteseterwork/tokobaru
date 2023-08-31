7b89b59eec56c94b99f1962feaf7ddc7fb42f48c
hilalmustofa <hilal.mustofa90@gmail.com>
2023-08-31 14:53:49 +0700

    revert back credentials include

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -4,23 +4,9 @@ export async function fetchData(url: string) {
   }
   
   export async function fetchWithToken(url: string, options: RequestInit = {}): Promise<any> {
-    const cookies = document.cookie;
-    const headers: HeadersInit = {
-      ...options.headers,
-      Cookie: cookies,
-    };
-  
-    const headersObject = headers as Record<string, string>;
-    const headersString = Object.keys(headersObject)
-      .map(key => `${key}: ${headersObject[key]}`)
-      .join('\n');
-  
-    console.log('Headers:', headersString);
-  
     const response = await fetch(url, {
       ...options,
-      credentials: 'include',
-      headers: headers,
+      credentials: 'include', 
     });
   
     if (!response.ok) {
