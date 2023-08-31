4b7f26149c05881f88748b135f2900d1040391b6
hilalmustofa <hilal.mustofa90@gmail.com>
2023-08-31 16:48:37 +0700

    add customFetch

--- utils/fetcher.ts
+++ utils/fetcher.ts
@@ -1,21 +1,11 @@
+import { customFetch } from './customFetch';
+
 export async function fetchData(url: string) {
     const res = await fetch(url);
     return res.json();
   }
-  
-  export async function fetchWithToken(url: string, options: RequestInit = {}): Promise<any> {
-    const response = await fetch(url, {
-      ...options,
-      credentials: 'include', 
-    });
-  
-    if (!response.ok) {
-      throw new Error(`Request failed with status ${response.status}`);
-    }
-  
-    const data = await response.json();
-    return data;
-  }
-  
-  
-  
+
+export async function fetchWithToken(url: string, options: RequestInit = {}) {
+  return customFetch(url, options);
+}
+
