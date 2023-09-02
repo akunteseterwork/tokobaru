c1a8aefe56ed447120ffccc49691bbace202d308
hilalmustofa <hilal.mustofa90@gmail.com>
2023-09-02 15:25:48 +0700

    remove enable system

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         />
       </head>
       <body className="font-inter">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light">
           <ThemeSwitcher />
           {children}
         </ThemeProvider>
