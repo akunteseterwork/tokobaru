f85f8f01cbc5bf358d54263dde46ef77c8d46515
hilalmustofa <hilal.mustofa90@gmail.com>
2023-09-02 23:10:57 +0700

    fix pagination

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
           onPageChange={handlePageChange}
         />
       </div>
-      <div className={`flex justify-center mt-4 mr-10 ${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-100'}`}>
+      <div className={`flex justify-center mt-4 md:mr-8 sm:mr-8 xs:mr8 lg:mr-10 ${theme === 'dark' ? 'bg-zinc-800' : 'bg-gray-100'}`}>
         <div className="pagination-container text-sm mb-4">
           <button
             className={`text-blue-500 px-2 py-1 rounded ${currentPage === 1 ? "opacity-50 cursor-not-allowed" : ""
