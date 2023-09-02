91759c90d503960d8c2178e1dda6de88833e0a68
hilalmustofa <hilal.mustofa90@gmail.com>
2023-09-02 23:05:53 +0700

    change mr-10

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      spacing: {
+        '10': '6.5rem',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
