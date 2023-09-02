fadf5292c0f4012cb35e8aa48c9fc87d21be98f6
hilalmustofa <hilal.mustofa90@gmail.com>
2023-09-02 16:12:07 +0700

    remove useEffect test

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import "./globals.css";
 import React , { useEffect } from 'react';
 import { ThemeProvider } from "@/app/themeProvider";
 import { ThemeSwitcher } from "@/app/themeSwitcher";
-import { useTheme } from "next-themes";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const { theme } = useTheme();
 
   return (
     <html lang="en">
--- app/page.tsx
+++ app/page.tsx
@@ -28,16 +28,6 @@ export default function Home() {
     setCurrentPage(page);
   };
 
-  useEffect(() => {
-    if (theme === 'dark') {
-      document.body.classList.remove('bg-gray-100');
-      document.body.classList.add('dark', 'bg-zinc-800', 'font-inter');
-    } else {
-      document.body.classList.remove('dark', 'bg-zinc-800', 'font-inter');
-      document.body.classList.add('bg-gray-100');
-    }
-  }, [theme]);
-
   if (error) return <ErrorMessage title="Error Loading Data" message="Unable to fetch data. Please try again later." />;
   if (!data)
     return (
